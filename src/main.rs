00d7f5bedd7599c5379362f435a00d72b305948f
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-04 00:01:59 +0100

    Printing board nicely

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use Tile::*;
 #[derive(Debug)]
 pub struct GameState {
     tiles: [Tile; 24],
+    captured: [u8; 2],
 }
 
 impl GameState {
@@ -43,26 +44,63 @@ impl GameState {
                 Empty,
                 Dark(2),
             ],
+            captured: [0, 0],
         }
     }
 }
 
 impl Display for GameState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "╔═════════════════╦═════════════════╗")?;
         for i in 0..12 {
+            write!(f, "║")?;
             match self.tiles[i] {
-                Empty => (),
-                Light(n) => (),
-                Dark(n) => (),
+                Empty => print!("---------------"),
+                t => {
+                    let (col, n) = match t {
+                        Light(n) => ("\x1b[33m", n),
+                        Dark(n) => ("\x1b[36m", n),
+                        Empty => unreachable!(),
+                    };
+
+                    let circles = "●".repeat(n as usize);
+                    let colored = format!("{col}{circles}\x1b[0m");
+                    let dashes = "-".repeat(15 - n as usize);
+                    write!(f, "{colored}{dashes}")?;
+                }
+            }
+
+            write!(f, "  ║  ")?;
+
+            match self.tiles[23 - i] {
+                Empty => print!("---------------"),
+                t => {
+                    let (col, n) = match t {
+                        Light(n) => ("\x1b[33m", n),
+                        Dark(n) => ("\x1b[36m", n),
+                        Empty => unreachable!(),
+                    };
+
+                    let circles = "●".repeat(n as usize);
+                    let colored = format!("{col}{circles}\x1b[0m");
+                    let dashes = "-".repeat(15 - n as usize);
+                    write!(f, "{dashes}{colored}")?;
+                }
+            }
+
+            if i == 5 {
+                writeln!(f, "║\n╠═════════════════╬═════════════════╣")?;
+            } else {
+                writeln!(f, "║")?;
             }
         }
 
-        Ok(())
+        writeln!(f, "╚═════════════════╩═════════════════╝")
     }
 }
 
 fn main() {
     let state = GameState::new();
 
-    println!("{state:?}");
+    println!("{state}");
 }
