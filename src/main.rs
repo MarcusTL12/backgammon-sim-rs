d9ff62639939e1c0efca50611eed034f3e1516dd
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-03 23:00:10 +0100

    coded starting tiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Tile {
+    Empty,
+    Light(u8),
+    Dark(u8),
+}
+
+use Tile::*;
+
+#[derive(Debug)]
+pub struct GameState {
+    tiles: [Tile; 24],
+}
+
+impl GameState {
+    pub fn new() -> Self {
+        Self {
+            tiles: [
+                Light(2),
+                Empty,
+                Empty,
+                Empty,
+                Empty,
+                Dark(5),
+                Empty,
+                Dark(3),
+                Empty,
+                Empty,
+                Empty,
+                Light(5),
+                Dark(5),
+                Empty,
+                Empty,
+                Empty,
+                Light(3),
+                Empty,
+                Light(5),
+                Empty,
+                Empty,
+                Empty,
+                Empty,
+                Dark(2),
+            ],
+        }
+    }
+}
+
+impl Display for GameState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..12 {
+            match self.tiles[i] {
+                Empty => (),
+                Light(n) => (),
+                Dark(n) => (),
+            }
+        }
+
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = GameState::new();
+
+    println!("{state:?}");
 }
