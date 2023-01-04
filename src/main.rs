1eda5fe6b2850fbc686536d8ed6868d73b794028
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-01-04 12:44:00 +0100

    get single moves done

--- src/main.rs
+++ src/main.rs
@@ -163,6 +163,7 @@ impl GameState {
         &self,
         turn: bool, // false => Light, true => Dark
         die: u8,
+        wasteful: bool,
         moves: &mut Vec<[u8; 2]>,
     ) {
         moves.clear();
@@ -171,23 +172,62 @@ impl GameState {
 
         let move_dir = if turn { -1 } else { 1 };
 
-        if self.is_all_home()[turn_ind] {
-            // TODO: Also check for moves that move out
-        }
+        if self.captured[turn_ind] == 0 {
+            for i in 0..24 {
+                if let (Light(_), false) | (Dark(_), true) =
+                    (self.tiles[i], turn)
+                {
+                    if let (Some(Empty), _)
+                    | (Some(Light(_)), false)
+                    | (Some(Dark(_)), true) = (
+                        self.tiles.get(
+                            (i as isize + die as isize * move_dir) as usize,
+                        ),
+                        turn,
+                    ) {
+                        moves.push([
+                            i as u8,
+                            (i as isize + die as isize * move_dir) as u8,
+                        ]);
+                    }
+                }
+            }
 
-        for i in 0..24 {
-            if let (Light(_), false) | (Dark(_), true) = (self.tiles[i], turn) {
-                if let (Some(Empty), _)
-                | (Some(Light(_)), false)
-                | (Some(Dark(_)), true) = (
-                    self.tiles
-                        .get((i as isize + die as isize * move_dir) as usize),
-                    turn,
-                ) {
-                    moves.push([
-                        i as u8,
-                        (i as isize + die as isize * move_dir) as u8,
-                    ]);
+            if self.is_all_home()[turn_ind] {
+                let die_pos = if turn {
+                    23 - die as usize
+                } else {
+                    die as usize
+                };
+
+                if let (Light(_), false) | (Dark(_), true) =
+                    (self.tiles[die_pos], turn)
+                {
+                    moves.push([die_pos as u8, 255]);
+                }
+
+                // At this point if no moves has been pushed,
+                // we can check for 'wasteful' moves.
+
+                if wasteful && moves.is_empty() {
+                    let mut i = die_pos as isize;
+                    while let Some(t) = self.tiles.get(i as usize) {
+                        if let (Light(_), false) | (Dark(_), true) = (t, turn) {
+                            moves.push([i as u8, 255]);
+                            break;
+                        }
+
+                        i += move_dir;
+                    }
+                }
+            }
+        } else {
+            let off = turn_ind * 18;
+            for i in off..off + 6 {
+                if let (Empty, _) | (Light(_), false) | (Dark(_), true) =
+                    (self.tiles[i], turn)
+                {
+                    moves.push([255, i as u8]);
                 }
             }
         }
